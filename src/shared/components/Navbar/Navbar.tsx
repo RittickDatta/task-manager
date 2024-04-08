52af67edf2d2d886c0f7f80b6ccb6bf3e288f083
Rittick Datta <drittick@gmail.com>
2024-04-08 17:18:51 +0200

    Add MUI

---
+++ src/shared/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Button, styled } from '@mui/material';
+
+const Navbar = () => {
+  //   const TestButton = styled(Button)(({theme}) => ({
+  //     padding: theme.spacing(2),
+  //   }));
+
+  return <div></div>;
+};
+
+export default Navbar;
