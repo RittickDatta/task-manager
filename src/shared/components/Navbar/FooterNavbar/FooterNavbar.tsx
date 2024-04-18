b702fb996dbab81a65b2471297c572716441ff80
Rittick Datta <drittick@gmail.com>
2024-04-18 12:54:01 +0200

    Add Footer Navbar;

--- src/shared/components/Navbar/FooterNavbar/FooterNavbar.tsx
+++ src/shared/components/Navbar/FooterNavbar/FooterNavbar.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { IoIosJet, IoMdPaperPlane } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+import "./FooterNavbar.scss";
 
 const FooterNavbar = () => {
-  return <div>Footer Navbar</div>;
+  return (
+    <nav className="footer-navbar">
+      <span className="logo-footer">
+        <IoIosJet /> Task Center
+      </span>
+      <ul className="nav-items-footer">
+        <li>
+          <NavLink to="/home" className="nav-item-footer">
+            <IoMdPaperPlane className="nav-icon-footer" /> Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/tasks" className="nav-item-footer">
+            <IoMdPaperPlane className="nav-icon-footer" /> Tasks
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/analytics" className="nav-item-footer">
+            <IoMdPaperPlane className="nav-icon-footer" /> Analytics
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default FooterNavbar;
